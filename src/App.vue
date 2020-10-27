653c50ed9cef6a5e2df7293656595b3e44762f26
sebastian959 <spaezrivera@gmail.com>
2020-10-27 13:21:44 -0500

    Adding files

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<template>
+<v-app>
+  <v-app-bar app color="primary" dark>
+    <div id="app">
+      <v-container>
+        <v-layout row class="text-xs-center">
+          <v-flex xs3 style="background-image: url('http://cdn.wallpapersafari.com/7/86/gqiGH7.jpg')">
+            <v-card height="500px"></v-card>
+          </v-flex>
+          <v-flex xs4 class="grey lighten-4">
+            <v-container style="position: relative;top: 13%;" class="text-xs-center">
+              <v-card flat>
+                <v-card-title primary-title>
+                  <h4>Login</h4>
+                </v-card-title>
+                <v-form>
+                  <v-text-field prepend-icon="person" name="Username" label="Username"></v-text-field>
+                  <v-text-field prepend-icon="lock" name="Password" label="Password" type="password"></v-text-field>
+                  <v-card-actions>
+                    <v-btn primary large block>Login</v-btn>
+                  </v-card-actions>
+                </v-form>
+              </v-card>
+            </v-container>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </div>
+
+    <v-btn>
+      <router-link to="/">Home</router-link>
+    </v-btn>
+    <v-btn>
+      <router-link to="/about">About</router-link>
+    </v-btn>
+    <v-btn>
+      <router-link to="/login">Login</router-link>
+    </v-btn>
+
+
+  </v-app-bar>
+
+  <v-content>
+    <router-view />
+  </v-content>
+</v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+
+  components: {
+
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/views/Login.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+  <v-container>
+    <v-layout row class="text-xs-center">
+      <v-flex xs3 style="background-image: url('http://cdn.wallpapersafari.com/7/86/gqiGH7.jpg')">
+        <v-card height="500px"></v-card>
+      </v-flex>
+      <v-flex xs4 class="grey lighten-4">
+        <v-container style="position: relative;top: 13%;" class="text-xs-center">
+          <v-card flat>
+            <v-card-title primary-title>
+              <h4>Login</h4>
+            </v-card-title>
+            <v-form>
+            <v-text-field prepend-icon="person" name="Username" label="Username"></v-text-field>
+            <v-text-field prepend-icon="lock" name="Password" label="Password" type="password"></v-text-field>
+            <v-card-actions>
+              <v-btn primary large block>Login</v-btn>
+            </v-card-actions>
+            </v-form>
+          </v-card>
+        </v-container>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</div>
+</template>
+
+<script>
+import auth from "@/logic/auth";
+export default {
+  data: () => ({
+    email: "",
+    password: "",
+    showPassword: false,
+    error: false
+  }),
+  methods: {
+    async login() {
+      try {
+        await auth.login(this.email, this.password);
+        this.$router.push("/");
+      } catch (error) {
+        this.error = true;
+      }
+    }
+  }
+};
+</script>
